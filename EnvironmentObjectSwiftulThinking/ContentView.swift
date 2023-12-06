87dfcd3688b3a47ab72b858d872ee0b81827cacb
Ricardo de Aagostini <ricardoagneto@gmail.com>
2023-12-06 16:22:24 -0300

    first commit

---
+++ EnvironmentObjectSwiftulThinking/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  EnvironmentObjectSwiftulThinking
+//
+//  Created by Ricardo de Agostini Neto on 06/12/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ EnvironmentObjectSwiftulThinking/EnvironmentObjectSwiftulThinkingApp.swift
@@ -0,0 +1,17 @@
+//
+//  EnvironmentObjectSwiftulThinkingApp.swift
+//  EnvironmentObjectSwiftulThinking
+//
+//  Created by Ricardo de Agostini Neto on 06/12/23.
+//
+
+import SwiftUI
+
+@main
+struct EnvironmentObjectSwiftulThinkingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
